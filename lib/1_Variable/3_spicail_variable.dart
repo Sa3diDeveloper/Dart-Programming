99fb067bad121ec9c3bde9649083d7c0724c9e45
Saadi Programmer <sa3dopro1231239@gmail.com>
2023-01-08 20:32:27 +0300

    Variable In Dart Programming

---
+++ lib/1_Variable/3_spicail_variable.dart
@@ -0,0 +1,9 @@
+void main() {
+   var list=<int>[2, 4, 6];
+                  
+  var set=<dynamic>{2, 4.5, true, 'Hello'};
+  var map=<String, int>{'Ali': 50, 'Mohammad': 90};
+  print('list=$list');
+  print('set=$set');
+  print('map=$map');
+}
