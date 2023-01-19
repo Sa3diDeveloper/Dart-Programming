e25c0052766d091b63c8abcbf2b121f867f4bbe4
Saadi Programmer <sa3dopro1231239@gmail.com>
2023-01-19 21:43:39 +0300

    List Method

--- lib/4_List Method/set.dart
+++ lib/4_List Method/set.dart
@@ -1,13 +1,12 @@
 void main() {
-  var x={1,2,3,4,5,6,6};
-
+  var x = {1, 2, 3, 4, 5, 6, 6};
 
   print(x.elementAt(1));
   print(x.contains(9));
-print("***********************");
-var j={1,5,6,7,8};
-var y={2,3,4,6,9};
-print(j.intersection(y));
-print(j.union(y));
-print(y.difference(j));
+  print("***********************");
+  var j = {1, 5, 6, 7, 8};
+  var y = {2, 3, 4, 6, 9};
+  print(j.intersection(y));
+  print(j.union(y));
+  print(y.difference(j));
 }
