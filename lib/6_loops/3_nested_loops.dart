be062959cf10c4a323f605746d668091a0d2a316
Saadi Programmer <sa3dopro1231239@gmail.com>
2023-01-28 18:09:41 +0300

    Nasted loops

--- lib/6_loops/3_nested_loops.dart
+++ lib/6_loops/3_nested_loops.dart
@@ -1,9 +1,9 @@
 void main() {
-  // for (var i = 1; i < 11; i++) {
-  //   for (var j = 1; j < 11; j++) {
-  //     print('$i * $j = ${i*j}');
-  //   }
-  // }
+  for (var i = 1; i < 11; i++) {
+    for (var j = 1; j < 11; j++) {
+      print('$i * $j = ${i*j}');
+    }
+  }
 
   var i = 1;
   do {
