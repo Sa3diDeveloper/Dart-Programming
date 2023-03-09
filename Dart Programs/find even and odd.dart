bb9b9542f8168096ae2c5d07ddbd2ff3ccadd665
Sa3di Programmer <sa3dopro1231239@gmail.com>
2023-03-09 14:37:56 +0300

    Dart Programs

---
+++ Dart Programs/find even and odd.dart
@@ -0,0 +1,17 @@
+void main() {
+  var evenNumbers = const [1, -2, 3, 42].where((number) => number.isEven);
+
+  for (final number in evenNumbers) {
+    print('$number is even.');
+  }
+
+  if (evenNumbers.any((number) => number.isNegative)) {
+    print('evenNumbers contains negative numbers.');
+  }
+
+  // If no element satisfies the predicate, the output is empty.
+  var largeNumbers = evenNumbers.where((number) => number > 1000);
+  if (largeNumbers.isEmpty) {
+    print('largeNumbers is empty!');
+  }
+}
