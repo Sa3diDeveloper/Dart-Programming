c6972870526c8237cc17b9099df46d509056d045
Sa3di Progarmmer <125812723+Sa3diDeveloper@users.noreply.github.com>
2023-03-15 20:02:40 +0300

    Delete Basics Of Dart directory

--- Basics Of Dart/14_input1.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:io';
-
-void main(){
-  print('Enter Your Birth Year:');
-  var birth_year = stdin.readLineSync();
-  var age = DateTime.now().year-int.parse(birth_year!);
-  print('Your Age is $age Years old');
-}
