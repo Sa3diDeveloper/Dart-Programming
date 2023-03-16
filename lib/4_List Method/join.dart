52cf620163ef0c7db85fc08354daca6e30e4dd64
Sa3di Progarmmer <125812723+Sa3diDeveloper@users.noreply.github.com>
2023-03-16 20:42:03 +0300

    Delete lib directory

--- lib/4_List Method/join.dart
+++
@@ -1,7 +0,0 @@
-void main() {
-    List<String> values = [' dart', 'flutter ', 'programming'];
-  // String result = values
-  //     .map((val) => val.trim())
-  //     .join(';');
-  print(values.join(";").trimRight());
-}
