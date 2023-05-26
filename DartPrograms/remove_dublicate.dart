4b24d1a55d98cdf15accae666f108b42ce930009
sa3diProgrammer <sa3dideveloper@gmail.com>
2023-05-26 11:06:33 -0700

    remove dublicate numbers

--- DartPrograms/remove_dublicate.dart
+++ DartPrograms/remove_dublicate.dart
@@ -12,4 +12,11 @@ List<String> countries = [
 var seen = Set<String>();
 List<String> uniquelist = countries.where((country) => seen.add(country)).toList();
 print(uniquelist);
+
+//!number
+List<int> numbers = [1,2,1,3,3,5,4,5];
+
+var seenint = Set<String>();
+List<int> uniquenum = numbers.where((numone) => seen.add(numone.toString())).toList();
+print(uniquenum);
 }
