c6b5ced9c3eb9d85c3a1e3d59d7afa3b7b44c0d8
sa3diProgrammer <sa3dideveloper@gmail.com>
2023-06-06 01:37:43 -0700

    record syntax 1

--- OOP/record/record_suntax.dart
+++ OOP/record/record_suntax.dart
@@ -1,4 +1,18 @@
 main() {
   var record = ('first', a: 2, b: true, 'last');
   print(record.a);
+
+  // Record type annotation in a variable declaration:
+  (String, int) record1;
+
+// Initialize it with a record expression:
+  record1 = ('A string', 123);
+  print(record1);
+
+
+  // Record type annotation in a variable declaration:
+({int a, bool b}) record2;
+
+// Initialize it with a record expression:
+record2 = (a: 123, b: true);
 }
