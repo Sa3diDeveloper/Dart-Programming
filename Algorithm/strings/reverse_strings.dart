f40d97d46ee2ecf21f3462660ce15d125ccb1060
sa3diProgrammer <sa3dideveloper@gmail.com>
2023-06-06 23:52:32 -0700

    reverse strings 1

--- Algorithm/strings/reverse_strings.dart
+++ Algorithm/strings/reverse_strings.dart
@@ -1,8 +1,17 @@
 main() {
-  var rev = reverse('saadi');
-  print(rev);
+  // var rev = reverse('saadi');
+  // print(rev);
+  print(reverseString2('saadi'));
 }
 
 String reverse(String str) {
   return str.split('').reversed.join();
+}
+
+String reverseString2(String str) {
+  String reverse = '';
+  for (int i = str.length-1; i >= 0; i--) {
+    reverse += str[i];
+  }
+  return reverse;
 }
