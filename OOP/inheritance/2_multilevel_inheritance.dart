57d08b53a38c0930a4ee66344334f0893e5468cc
sa3diProgrammer <sa3dideveloper@gmail.com>
2023-06-18 00:58:28 -0700

    Inheritance 1

--- OOP/inheritance/2_multilevel_inheritance.dart
+++ OOP/inheritance/2_multilevel_inheritance.dart
@@ -1,2 +1,27 @@
-//!In the multiple inheritance, a subclass is inherited by another subclass or 
-//!creates the chaining of inheritance. 
+class Bird {
+  void fly() {
+    print("The bird can fly");
+  }
+}
+
+// Inherits the super class
+class Parrot extends Bird {
+  void speak() {
+    print("The parrot can speak");
+  }
+}
+
+// Inherits the Parrot base class
+class Eagle extends Parrot {
+  void vision() {
+    print("The eagle has a sharp vision");
+  }
+}
+
+void main() {
+  // Creating object of the child class
+  Eagle e = new Eagle();
+  e.speak();
+  e.fly();
+  e.vision();
+}
