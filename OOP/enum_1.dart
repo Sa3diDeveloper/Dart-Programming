93243abbb549b2fa8cc02eef9b8ac2fef450650f
sa3di Programmer <sa3dideveloper01@gmail.com>
2023-06-24 03:58:15 -0700

    enum

--- OOP/enum_1.dart
+++ OOP/enum_1.dart
@@ -1,18 +1,37 @@
 void main() {
-  print(info(Color.red));
-  print(Color.values[1]);
-  print(Color.values.length);
+  print(Vehicle.bus.passengers);
 }
 
-enum Color { red, blue, black }
+enum Vehicle implements Comparable<Vehicle> {
+  car(tires: 4, passengers: 5, carbonPerKilometer: 400),
+  bus(tires: 6, passengers: 50, carbonPerKilometer: 800),
+  bicycle(tires: 2, passengers: 1, carbonPerKilometer: 0);
 
-String info(Color s) {
-  switch (s) {
-    case Color.red:
-      return 'red';
-    case Color.blue:
-      return 'blue';
-    case Color.black:
-      return 'black';
-  }
+  const Vehicle({
+    required this.tires,
+    required this.passengers,
+    required this.carbonPerKilometer,
+  });
+
+  final int tires;
+  final int passengers;
+  final int carbonPerKilometer;
+
+  int get carbonFootprint => (carbonPerKilometer / passengers).round();
+
+  bool get isTwoWheeled => this == Vehicle.bicycle;
+
+  @override
+  int compareTo(Vehicle other) => carbonFootprint - other.carbonFootprint;
 }
+
+// String info(Color s) {
+//   switch (s) {
+//     case Color.red:
+//       return 'red';
+//     case Color.blue:
+//       return 'blue';
+//     case Color.black:
+//       return 'black';
+//   }
+// }
