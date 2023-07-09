20dfca3d8276a00b21ec216f7bc413fc11a567f4
sa3di Programmer <sa3dideveloper01@gmail.com>
2023-07-09 00:31:01 -0700

    generic in dart

--- OOP/Genric.dart
+++ OOP/Genric.dart
@@ -4,18 +4,18 @@ class Pair<T, U> {
   Pair(this.first, this.second);
 }
 
-T max<T>(List<T> list) {
-  T maxValue = list[0];
-  for (T value in list) {
-    if (maxValue! == value!) {
-      maxValue = value;
-    }
+max<T, B>(T a, B b) {
+  if (a == b) {
+    return a;
+  } else {
+    return b;
   }
-  return maxValue;
 }
+
 void main() {
   var pair = Pair<int, String>(1, "Hello");
   var pair1 = Pair<double, String>(7.1, "Hello");
+  print(max(1, 7));
   print(pair.first); // 1
   print(pair1.first); // Hello
 }
